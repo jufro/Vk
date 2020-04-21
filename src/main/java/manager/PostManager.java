bc868b044134989c42d1a14c35061c6e3e160a07
Yuliya Frolova <jupunkevich@gmail.com>
2020-04-21 21:46:46 +0300

    Changed object Search

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -5,7 +5,7 @@ import domain.Post;
 public class PostManager {
         private Post[] posts;
 
-    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly,
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly,
                          int count, int offset) {
         // TODO: add logic
         return null;
